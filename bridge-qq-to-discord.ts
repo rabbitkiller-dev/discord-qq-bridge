1b0c03b59ace337533635e5db82e512c5121c410
rabbitkiller <243249439@qq.com>
2020-11-29 13:22:07 +0800

    feat: 支持回复消息 fix: 修复discord没有发送图片就报错

--- bridge-qq-to-discord.ts
+++ bridge-qq-to-discord.ts
@@ -1,16 +1,19 @@
 import {Client, MessageAttachment, WebhookMessageOptions} from "discord.js";
-import {App, CQCode} from 'koishi';
+import {App, CQCode, RawSession} from 'koishi';
 import config from "./koishi.config";
+import axios from "axios";
 import * as md5 from "md5";
 import * as log from "./utils/log5";
 
 const {sysLog} = require('./utils/sysLog'); // sysLog 保存日志
-
+let discord: Client;
+let koishi: App
 export default async function (ctx: {
     discord: Client,
     koishi: App
 }) {
-    const {koishi, discord} = ctx;
+    koishi = ctx.koishi;
+    discord = ctx.discord;
     koishi.group(...config.bridges.map(b => b.qqGroup)).on('message', async (msg) => {
         // 查看有没有连接配置
         const bridge = config.bridges.find((bridge) => bridge.qqGroup === msg.groupId);
@@ -18,7 +21,12 @@ export default async function (ctx: {
             return;
         }
         // 把cq消息解码成对象
-        const cqMessages = CQCode.parseAll(msg.message);
+        let messageContent = msg.message;
+        // 处理回复
+        messageContent = await handlerReply(messageContent);
+        // 处理at
+        messageContent = await handlerAt(messageContent, msg);
+        const cqMessages = CQCode.parseAll(messageContent);
         // 获取webhook
         const webhook = await discord.fetchWebhook(bridge.discord.id, bridge.discord.token);
         for (const cqMsg of cqMessages) {
@@ -51,4 +59,50 @@ export default async function (ctx: {
         }
         sysLog('⇿', 'QQ信息已推送到Discord', msg.sender.nickname, msg.message)
     });
+}
+
+function resolveBrackets(msg) {
+    msg = msg.replace(new RegExp('&#91;', 'g'), '[').replace(new RegExp('&#93;', 'g'), ']')
+    return msg
+}
+
+// 处理回复
+async function handlerReply(message: string): Promise<string> {
+    let cqMessages = CQCode.parseAll(message);
+    if (cqMessages[0] && cqMessages[0]['type'] === 'reply') {
+        const reply: any = cqMessages[0];
+        const response = await axios.get(config.server + '/get_msg', {
+            params: {
+                message_id: reply.data.id
+            }
+        })
+        const result = response.data;
+        let replyMsg = ``;
+        if (result.status === 'ok') {
+            const replyTime = new Date(result.data.time * 1000);
+            const replyDate = `${replyTime.getHours()}:${replyTime.getMinutes()}:${replyTime.getSeconds()}`;
+
+            replyMsg = result.data.message
+            replyMsg = resolveBrackets(replyMsg)
+            replyMsg = replyMsg.split('\n').join('\n> ')
+            replyMsg = '> ' + replyMsg + '\n'
+            replyMsg = `> **__回复 ${result.data.sender.nickname} 在 ${replyDate} 的消息__**\n` + replyMsg
+        }
+        cqMessages.splice(0, 2);
+        cqMessages = [replyMsg, ...cqMessages];
+    }
+    return CQCode.stringifyAll(cqMessages);
+}
+
+// 把表情解析成cq:image
+async function handlerAt(message: string, ctx: RawSession<'message'>): Promise<string> {
+    let cqMessages = CQCode.parseAll(message);
+    cqMessages = await Promise.all(cqMessages.map(async (cqMsg) => {
+        if (typeof cqMsg === 'string' || cqMsg.type !== 'at') {
+            return cqMsg;
+        }
+        const user = await koishi.bots[0].getGroupMemberInfo(518986671, 243249439);
+        return `\`@${user.card || user.nickname}(${user.userId})\``
+    }));
+    return CQCode.stringifyAll(cqMessages);
 }
